407af35e4db6885135c9f6fefcad8efc1f39435e
Parfait2026 <2547117@cegeplimoilou.ca>
2026-03-27 14:29:38 -0400

    Création de la classe OuvragePapier enfant de Ouvrage et abstraction de la classe Ouvrage sans correction des erreurs apparues

---
+++ src/livres/OuvragePapier.java
@@ -0,0 +1,38 @@
+package livres;
+
+import java.time.LocalDate;
+
+public class OuvragePapier extends Ouvrage {
+    private int nombrePages;
+
+
+    public OuvragePapier(String titre, Auteur auteur, Format type, LocalDate date, int nombreExemplaires, int nombrePages) {
+        super(titre, auteur, type, date, nombreExemplaires);
+        this.nombrePages = nombrePages;
+    }
+
+    public OuvragePapier(String titre, Auteur auteur, Format type, int nombrePages) {
+        super(titre, auteur, type);
+        this.nombrePages = nombrePages;
+    }
+
+    public OuvragePapier(String titre, Auteur auteur, int nombrePages) {
+        super(titre, auteur);
+        this.nombrePages = nombrePages;
+    }
+
+    public int getNombrePages() {
+        return nombrePages;
+    }
+
+    public void setNombrePages(int nombrePages) {
+        this.nombrePages = nombrePages;
+    }
+
+    @Override
+    public String toString() {
+        return "OuvragePapier{" +
+                "nombrePages=" + nombrePages +
+                '}';
+    }
+}
