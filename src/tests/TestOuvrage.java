c1851a288dea85182d8f732149ff08c9096b1e28
Grace2372 <2359107@cegeplimoilou.ca>
2026-03-27 15:24:03 -0400

    Modification de la classe TestOuvrage des instantions des objects Ouvrages pour appliquer du polymorphisme

--- src/tests/TestOuvrage.java
+++ src/tests/TestOuvrage.java
@@ -4,10 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import livres.Ouvrage;
-import livres.Auteur;
-import livres.Pays;
-import livres.Serie;
+import livres.*;
 
 /**
  * CoursPOO 1
@@ -34,18 +31,18 @@ public class TestOuvrage {
 
         //Voici une partie des tests! Il faut en ajouter, pour les fonctionnalités non testées!
         System.out.println("-----Test des constructeurs d'ouvrage et des diverses validations-----------");
-        Ouvrage livreA = new Ouvrage("Titre assez long", john);
+        Ouvrage livreA = new OuvrageAudio("Titre assez long", john, Ouvrage.Format.AUDIO,LocalDate.now(),10,235, OuvrageAudio.FormatAudio.ANALOGIQUE);
         System.out.println(livreA);
-        Ouvrage livreB = new Ouvrage("Ti", john);
+        Ouvrage livreB = new OuvrageVideo("Ti", john, Ouvrage.Format.VIDEO,LocalDate.now(),24,10,1.2);
         System.out.println(livreB);
-        Ouvrage livreC = new Ouvrage(null, john);
+        Ouvrage livreC = new OuvragePapier(null, john, Ouvrage.Format.PAPIER,LocalDate.now(),4,317);
         System.out.println(livreC);
 
         //bibliotheque.Auteur null et valeur par défaut de l'bibliotheque.Auteur
-        Ouvrage livreA1 = new Ouvrage("Titre assez long", null);
+        Ouvrage livreA1 = new OuvragePapier("Titre assez long", null, Ouvrage.Format.PAPIER,LocalDate.now(),20,120);
         System.out.println(livreA1);
         //bibliotheque.Auteur fonctionnel
-        Ouvrage livre1 = new Ouvrage("Tout va bien", albertine);
+        Ouvrage livre1 = new OuvrageAudio("Tout va bien", albertine, Ouvrage.Format.AUDIO,null,3,6, OuvrageAudio.FormatAudio.NUMERIQUE);
         System.out.println(livre1);
 
         //Date null et valeur par défaut de la date
@@ -55,15 +52,15 @@ public class TestOuvrage {
         System.out.println(livre1);
 
         //Test de la validation sur le nb d'exemplaires (valide et non valide)
-        Ouvrage livre2 = new Ouvrage("Tout va bien", albertine, Ouvrage.Format.AUDIO, LocalDate.now(), -10);
+        Ouvrage livre2 = new OuvragePapier("Tout va bien", albertine, Ouvrage.Format.PAPIER, LocalDate.now(), -10,190);
         System.out.println(livre2);
 
-        livre2 = new Ouvrage("Tout va bien", albertine, Ouvrage.Format.PAPIER, LocalDate.now(), 20);
+        livre2 = new OuvrageVideo("Tout va bien", albertine, Ouvrage.Format.VIDEO, LocalDate.now(), 20,7,3.4);
         System.out.println(livre2);
 
         System.out.println("\n-----Tests des méthodes acheter et vendre-----------");
 
-        Ouvrage livre3 = new Ouvrage("Musique du hasard", new Auteur("Paul", "Auster", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre3 = new OuvrageAudio("Musique du hasard", new Auteur("Paul", "Auster", new Pays("Etats-Unis", "USA")), Ouvrage.Format.AUDIO, LocalDate.now(), 5,12, OuvrageAudio.FormatAudio.ANALOGIQUE);
         System.out.println(livre3);
 
         livre3.acheter(5);
@@ -75,14 +72,14 @@ public class TestOuvrage {
         System.out.println("On peut vendre 10 livres? " + livre3.vendre(10));
         System.out.println(livre3);
 
-        Ouvrage livre4 = new Ouvrage("Test", new Auteur("A", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre4 = new OuvrageVideo("Test", new Auteur("A", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.VIDEO, LocalDate.now(), 5,30,4.5);
 
         System.out.println("\n-----Tests de la  méthode equals()-----------");
         //Deux ouvrages égaux
-        Ouvrage livre5 = new Ouvrage("Test", new Auteur("A", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, null, 5);
-        Ouvrage livre6 = new Ouvrage("Test", new Auteur("A", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, LocalDate.now(), 10);
+        Ouvrage livre5 = new OuvragePapier("Test", new Auteur("A", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, null, 5,467);
+        Ouvrage livre6 = new OuvragePapier("Test", new Auteur("A", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, LocalDate.now(), 10,600);
         //Un qui ne l'est pas
-        Ouvrage livre7 = new Ouvrage("Test", new Auteur("Z", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre7 = new OuvragePapier("Test", new Auteur("Z", "B", new Pays("Etats-Unis", "USA")), Ouvrage.Format.PAPIER, LocalDate.now(), 5,578);
 
         System.out.println("Test de la méthode equals d'bibliotheque.Ouvrage:" + livre4.equals(livre5));
         System.out.println("Test de la méthode equals d'bibliotheque.Ouvrage:" + livre4.equals(livre6));
@@ -155,10 +152,10 @@ public class TestOuvrage {
         Auteur auteur2 = new Auteur("Joseph", "Ignace", pays2);
 
         // ==== CRÉATION DES OUVRAGES ====
-        Ouvrage ouvrage1 = new Ouvrage("Disque d'or", auteur1);
-        Ouvrage ouvrage2 = new Ouvrage("Livre de l'année", auteur2);
-        Ouvrage ouvrage3 = new Ouvrage("Le Figarot", auteur1);
-        Ouvrage ouvrage4 = new Ouvrage("CNN", auteur2);
+        Ouvrage ouvrage1 = new OuvrageVideo("Disque d'or", auteur1, Ouvrage.Format.VIDEO,10,4.0);
+        Ouvrage ouvrage2 = new OuvragePapier("Livre de l'année", auteur2, Ouvrage.Format.PAPIER,920);
+        Ouvrage ouvrage3 = new OuvrageAudio("Le Figarot", auteur1, Ouvrage.Format.AUDIO,3, OuvrageAudio.FormatAudio.ANALOGIQUE);
+        Ouvrage ouvrage4 = new OuvrageAudio("CNN", auteur2, Ouvrage.Format.AUDIO,6, OuvrageAudio.FormatAudio.NUMERIQUE);
 
         System.out.println("====== AFFICHAGE DES SERIES ======");
         // ==== PREMIÈRE SÉRIE ===
@@ -184,8 +181,8 @@ public class TestOuvrage {
         Auteur auteur1 = new Auteur("Parfait", "Antoine", pays1);
         Auteur auteur2 = new Auteur("Joseph", "Ignace", pays2);
 
-        Ouvrage ouvrage1 = new Ouvrage("Le Figarot", auteur1);
-        Ouvrage ouvrage2 = new Ouvrage("CNN", auteur2);
+        Ouvrage ouvrage1 = new OuvrageVideo("Le Figarot", auteur1,16,5.6);
+        Ouvrage ouvrage2 = new OuvrageAudio("CNN", auteur2,20, OuvrageAudio.FormatAudio.ANALOGIQUE);
 
         System.out.println("====== ENLEVER DES OUVRAGES ======");
         // ==== ENLEVER OUVRAGE1 DANS SÉRIE1 ===
