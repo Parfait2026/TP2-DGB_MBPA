342b1941d957336b03490b7aaa94c107a7db1814
Grace2372 <2359107@cegeplimoilou.ca>
2026-03-27 14:25:24 -0400

    Implémentation d'une méthode testCreationPays() dans la classe TestOuvrage. création des Pays,setter un pays à un auteur puis afficher le pays de l'auteur.

---
+++ src/livres/OuvrageVideo.java
@@ -0,0 +1,50 @@
+package livres;
+
+import java.time.LocalDate;
+
+public class OuvrageVideo extends Ouvrage{
+    private int durerMinutes;
+    private  double tailleMb;
+
+    public OuvrageVideo(String titre, Auteur auteur, Format type, LocalDate date, int nombreExemplaires, int durerMinutes, double tailleMb) {
+        super(titre, auteur, type, date, nombreExemplaires);
+        this.durerMinutes = durerMinutes;
+        this.tailleMb = tailleMb;
+    }
+
+    public OuvrageVideo(String titre, Auteur auteur, Format type, int durerMinutes, double tailleMb) {
+        super(titre, auteur, type);
+        this.durerMinutes = durerMinutes;
+        this.tailleMb = tailleMb;
+    }
+
+    public OuvrageVideo(String titre, Auteur auteur, int durerMinutes, double tailleMb) {
+        super(titre, auteur);
+        this.durerMinutes = durerMinutes;
+        this.tailleMb = tailleMb;
+    }
+
+    public int getDurerMinutes() {
+        return durerMinutes;
+    }
+
+    public void setDurerMinutes(int durerMinutes) {
+        this.durerMinutes = durerMinutes;
+    }
+
+    public double getTailleMb() {
+        return tailleMb;
+    }
+
+    public void setTailleMb(double tailleMb) {
+        this.tailleMb = tailleMb;
+    }
+
+    @Override
+    public String toString() {
+        return "OuvrageVideo{" +
+                "durerMinutes=" + durerMinutes +
+                ", tailleMb=" + tailleMb +
+                '}';
+    }
+}
